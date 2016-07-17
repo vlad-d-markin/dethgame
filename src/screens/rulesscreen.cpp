0cd9eb930e2e6e8c805488f71ca4de39bf2eb456
AlineKl <al.kladieva@gmail.com>
2016-07-17 20:53:36 +0300

    Rules screen adapted to full screen mode

--- src/screens/rulesscreen.cpp
+++ src/screens/rulesscreen.cpp
@@ -6,7 +6,7 @@ RulesScreen::RulesScreen()
     setName("Rules screen");
     setEnable(true);
 
-    Gui::spTextLabel rules_label = new Gui::TextLabel();
+    rules_label = new Gui::TextLabel();
     rules_label->setText("Rules");
     TextStyle style = rules_label->getTextStyle();
     style.fontSize = 40;
@@ -15,18 +15,26 @@ RulesScreen::RulesScreen()
     style.font = m_resources->getResFont("font")->getFont();
     rules_label->setTextStyle(style);
     rules_label->setAnchor(0.5, 0.5);
-    rules_label->setPosition(this->getWidth()/2 - rules_label->getWidth()/2, 50);
+    //rules_label->setPosition(this->getWidth()/2 - rules_label->getWidth()/2, 50);
     rules_label->attachTo(this);
-    Gui::spTextLabel rules = new Gui::TextLabel();
+    rules = new Gui::TextLabel();
     rules->setText("There are no rules.\n\n\nBUT: To walk you should use W, A, S, D.\nTo attack - press Space.\n\nThat's all.\nBloodluck");
     style.fontSize = 35;
     style.hAlign = TextStyle::HALIGN_CENTER;
     rules->setTextStyle(style);
     rules->setAnchor(0.5,0.5);
-    rules->setPosition(getWidth()/2 + rules->getWidth()/2, 180);
+    //rules->setPosition(getWidth()/2 + rules->getWidth()/2, 180);
+	updateLayout();
     rules->attachTo(this);
 }
 
+void RulesScreen::updateLayout()
+{
+	int x, y;
+	SDL_GetWindowSize(core::getWindow(), &x, &y);
+	rules_label->setPosition(x / 2 - rules_label->getWidth() / 2, 50);
+	rules->setPosition(x / 2 + rules->getWidth() / 2, 180);
+}
 
 void RulesScreen::doUpdate(const UpdateState &us)
 {
